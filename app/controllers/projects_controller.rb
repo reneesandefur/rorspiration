a8abbba888df163865ffeda9c0d860191ed5ccb5
Renee Sandefur <reneersandefur@gmail.com>
2017-03-28 01:41:30 +0000

    added simple_form and worked on functionality

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,9 +2,19 @@ class ProjectsController < ApplicationController
     def index
         @projects = Project.all
     end
-    def show
-    end
+    
     def new
        @project = Project.new 
+    end
+    
+    def show
+        @project = Project.find(params[:id])
+    end
+    
+    def create
+        @project = Project.create
+        if @project.valid?
+            redirect_to project_path
+        end
     end
 end
